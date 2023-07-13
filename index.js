27b0b4b060b0ede514eb491916c7165a92c6f236
bomaboy <bomaboy1996@gmail.com>
2023-07-13 18:23:55 +0100

    project_Quiz1

--- index.js
+++ index.js
@@ -1,33 +1,160 @@
-let questions = document.getElementById("questions");
+// let question = document.getElementById("questions");
+// let option1 = document.getElementById("option1");
+// let option2 = document.getElementById("option2");
+// let option3 = document.getElementById("option3");
+// let option4 = document.getElementById("option4");
+// let loader = document.querySelector(".loader");
+// let qui = document.querySelector(".rightDiv");
+// let buttons = document.querySelectorAll(".button");
+// let progress = document.getElementById("progress");
+
+// let api =
+//    "https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple";
+
+// let arr = [];
+// let nextQuestion = 0;
+// fetch(api)
+//    .then((response) => response.json())
+//    .then((data) => {
+//       let quiz = data.results;
+//       quiz.map((item) => {
+//          let quizObj = {
+//             question: item.question,
+//             options: [...item.incorrect_answers, item.correct_answer],
+//             answer: item.correct_answer,
+//          };
+//          quizObj.options.sort((a) => Math.random() - 0.5);
+//          arr.push(quizObj);
+//       });
+//       arr.sort((a) => Math.random() - 0.5);
+//       question.innerHTML = arr[nextQuestion].question;
+//       option1.innerHTML = arr[nextQuestion].options[0];
+//       option2.innerHTML = arr[nextQuestion].options[1];
+//       option3.innerHTML = arr[nextQuestion].options[2];
+//       option4.innerHTML = arr[nextQuestion].options[3];
+//       loader.style.display = "none";
+//       qui.style.display = "flex";
+//    });
+
+// for (const button of buttons) {
+//    let btn = [option1, option2, option3, option4];
+//    button.onclick = function () {
+//       btn.map((butt) => (butt.disabled = true));
+//       if (button.innerText === arr[nextQuestion].answer) {
+//          button.style.backgroundColor = "green";
+//          button.style.color = "#f8cd0c";
+
+//          setTimeout(() => {
+//             button.style.backgroundColor = "#57636f";
+//             button.style.color = "white";
+//          }, 1000);
+//       } else {
+//          button.style.backgroundColor = "red";
+//          button.style.color = "black";
+
+//          setTimeout(() => {
+//             button.style.backgroundColor = "#57636f";
+//             button.style.color = "white";
+//          }, 1000);
+//      }
+//          setTimeout(() => {
+//             nextQuestion = nextQuestion + 1;
+//             progress.innerText = `Question ${nextQuestion + 1} of 10`;
+//             btn.map((butt) => (butt.disabled = false));
+//             question.innerHTML = arr[nextQuestion].question;
+//             option1.innerHTML = arr[nextQuestion].options[0];
+//             option2.innerHTML = arr[nextQuestion].options[1];
+//             option3.innerHTML = arr[nextQuestion].options[2];
+//             option4.innerHTML = arr[nextQuestion].options[3];
+//          2000);
+//      },
+//    };
+// }
+
+let question = document.getElementById("questions");
 let option1 = document.getElementById("option1");
 let option2 = document.getElementById("option2");
 let option3 = document.getElementById("option3");
 let option4 = document.getElementById("option4");
+let loader = document.querySelector(".loader");
+let qui = document.querySelector(".rightDiv");
+let buttons = document.querySelectorAll(".button");
+let progress = document.getElementById("progress");
+let score = document.getElementById("score");
+let scoreDiv = document.querySelector(".scoreDiv");
+let right = document.getElementById("right");
+let wrong = document.getElementById("wrong");
+let endgame = document.getElementById("endgame");
+let answerCount = 0;
 
 let api =
    "https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple";
 
+let arr = [];
+let nextQuestion = 0;
+
 fetch(api)
-   .then((Response) => Response.json())
+   .then((response) => response.json())
    .then((data) => {
-      console.log(data.results);
       let quiz = data.results;
-      let arr = [];
       quiz.map((item) => {
          let quizObj = {
             question: item.question,
             options: [...item.incorrect_answers, item.correct_answer],
-            answer: item.correct_answers,
+            answer: item.correct_answer,
          };
          quizObj.options.sort((a) => Math.random() - 0.5);
          arr.push(quizObj);
       });
       arr.sort((a) => Math.random() - 0.5);
-      questions.innerHTML = arr[0].question;
-      option1.innerHTML = arr[0].options[0];
-      option2.innerHTML = arr[0].options[1];
-      option3.innerHTML = arr[0].options[2];
-      option4.innerHTML = arr[0].options[3];
-      console.log(arr);
+      question.innerHTML = arr[nextQuestion].question;
+      option1.innerHTML = arr[nextQuestion].options[0];
+      option2.innerHTML = arr[nextQuestion].options[1];
+      option3.innerHTML = arr[nextQuestion].options[2];
+      option4.innerHTML = arr[nextQuestion].options[3];
+      loader.style.display = "none";
+      qui.style.display = "flex";
    });
-// });
+
+for (const button of buttons) {
+   let btn = [option1, option2, option3, option4];
+   button.onclick = function () {
+      btn.map((butt) => (butt.disabled = true));
+      if (button.innerText === arr[nextQuestion].answer) {
+         button.style.backgroundColor = "green";
+         button.style.color = "white";
+         right.play();
+         answerCount++;
+         setTimeout(() => {
+            button.style.backgroundColor = "#57636f";
+            button.style.color = "#f8cd0c";
+         }, 1000);
+      } else {
+         button.style.backgroundColor = "red";
+         button.style.color = "black";
+         wrong.play();
+         setTimeout(() => {
+            button.style.backgroundColor = "#57636f";
+            button.style.color = "#f8cd0c";
+         }, 1000);
+      }
+      setTimeout(() => {
+         nextQuestion = nextQuestion + 1;
+         progress.innerText = `Question ${nextQuestion + 1} of 20`;
+         btn.map((butt) => (butt.disabled = false));
+         question.innerHTML = arr[nextQuestion].question;
+         option1.innerHTML = arr[nextQuestion].options[0];
+         option2.innerHTML = arr[nextQuestion].options[1];
+         option3.innerHTML = arr[nextQuestion].options[2];
+         option4.innerHTML = arr[nextQuestion].options[3];
+      }, 2000);
+      setTimeout(() => {
+         if (nextQuestion == 19) {
+            qui.style.display = "none";
+            scoreDiv.style.display = "grid";
+            score.innerText = `${answerCount} / 20`;
+            endgame.play();
+         }
+      }, 3000);
+   };
+}
